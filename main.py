f242acb9ae2cfcebd9f0febf70eb52c2e23dde3e
Can <canozdal@hotmail.com>
2023-06-28 16:49:39 +0300

    Initial data flow

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -12,5 +16,20 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    url = "https://www.tff.org/default.aspx?pageID=198"
 
+    response = requests.get(url)
+    driver = webdriver.Chrome()
+    driver.get(url)
+    soup = BeautifulSoup(response.content,"html.parser")
+    score_table = soup.find(class_="s-table")
+    team_score = score_table.find_all("tr")
+    count= 0
+    for count in range(1,20):
+        if count < 10:
+            print(driver.find_element(By.ID,"ctl00_MPane_m_198_10561_ctnr_m_198_10561_grvACetvel_ctl0"+ str(count) + "_lnkTakim").text)
+        else:
+            print(driver.find_element(By.ID,"ctl00_MPane_m_198_10561_ctnr_m_198_10561_grvACetvel_ctl" + str(count) + "_lnkTakim").text)
+
+    driver.quit()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
